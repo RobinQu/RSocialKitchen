586a99299e4d036fc0c74e7169a3fb188163caa6
RobinQu <robinqu@gmail.com>
2013-06-20 17:37:10 +0800

    small fix; api summary

---
+++ Classes/SinaWeibo/SinaWeiboAPI.h
@@ -0,0 +1,15 @@
+//
+//  SinaWeiboAPI.h
+//  SocialAgentsKitchenSink
+//
+//  Created by Robin Qu on 13-6-19.
+//  Copyright (c) 2013年 Robin Qu. All rights reserved.
+//
+
+#ifndef SocialAgentsKitchenSink_SinaWeiboAPI_h
+#define SocialAgentsKitchenSink_SinaWeiboAPI_h
+
+
+static NSString *const WeiboUserInfoAPIPath = @"users/show.json";
+
+#endif
